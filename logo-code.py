c33b7cf3172ed055fb39220c8161fc0743941ff0
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-07-25 10:04:41 -0400

    Update README.md with new name

--- logo-code.py
+++ logo-code.py
@@ -4,18 +4,24 @@ from manim import *
 
 class Logo(Scene):
     def construct(self):
-        name = Text("adeshpande", color=RED).scale(0.5).set_stroke(color=RED)
+        name = Text("Aarush Deshpande", color=RED).scale(0.5).set_stroke(color=RED)
         username = Text("JasonGrace2282", color=BLUE).set_stroke(color=BLUE)
         group = VGroup(name, username).arrange(DOWN).scale(0.7)
-        self.play(Write(name), Write(username, reverse=True))
+        self.play(Write(name), Write(username, reverse=True), run_time=1)
         big_group = group.copy().scale(2)
         self.play(
             group.animate(rate_func=rate_functions.ease_out_bounce).scale(2), 
             Circumscribe(big_group, Circle)
         )
         self.wait()
-        self.play(Uncreate(group))
+        self.play(
+            Succession(
+                Unwrite(name),
+                Unwrite(username),
+                run_time=1,
+            )
+        )
 
 
-with tempconfig({"preview": True, "format": "gif", "transparent": True}):
+with tempconfig({"format": "gif", "transparent": True}):
     Logo().render()
